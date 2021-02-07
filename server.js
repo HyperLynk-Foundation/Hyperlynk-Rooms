eaf94adc3a867ba04ae8e0b57c6d55bc74607a54
Naman P <66672786+HyperLynk@users.noreply.github.com>
2021-02-07 18:59:03 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http")
 
 const server = http.createServer((req, res) => {})
 
-server.listen(3000, () => {
+server.listen(https://hyperlynk-rooms-again.vercel.app, () => {
     console.log("Listening on port 3000...")
 })
 
