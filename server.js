c59fc3844fcf1588e5a88f023c3ef394734fa8e6
Naman P <66672786+HyperLynk@users.noreply.github.com>
2021-02-07 19:03:06 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http")
 
 const server = http.createServer((req, res) => {})
 
-server.listen(, () => {
+server.listen(http, () => {
     console.log("Listening on port 3000...")
 })
 
